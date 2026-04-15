e390bfb3e0a60351caec21f6fe65b70d38bf5213
Anthony Hayward <antmannacho@gmail.com>
2026-04-15 00:39:17 -0400

    Update Expo dependencies and patch fmt iOS build

--- plugins/withProgressiveBlurNativeBuild.js
+++ plugins/withProgressiveBlurNativeBuild.js
@@ -45,6 +45,45 @@ def assert_progressive_blur_codegen!(ios_root)
     raise "ReactNativeBlurViewSpec is missing required native props: #{missing_tokens.join(', ')}"
   end
 end
+
+def apply_fmt_xcode_workaround!(installer)
+  base_header_path = File.join(installer.sandbox.root.to_s, 'fmt', 'include', 'fmt', 'base.h')
+
+  if File.exist?(base_header_path)
+    header_source = File.read(base_header_path)
+    override_marker = "#ifdef FMT_USE_CONSTEVAL\n// Use the provided definition.\n"
+    detection_needle = "// Detect consteval, C++20 constexpr extensions and std::is_constant_evaluated.\n#if !defined(__cpp_lib_is_constant_evaluated)\n"
+
+    unless header_source.include?(override_marker)
+      unless header_source.include?(detection_needle)
+        raise "Unable to locate fmt consteval detection block in #{base_header_path}"
+      end
+
+      header_source = header_source.sub(
+        detection_needle,
+        "// Detect consteval, C++20 constexpr extensions and std::is_constant_evaluated.\n#ifdef FMT_USE_CONSTEVAL\n// Use the provided definition.\n#elif !defined(__cpp_lib_is_constant_evaluated)\n"
+      )
+
+      File.write(base_header_path, header_source)
+    end
+  end
+
+  installer.pods_project.targets.each do |target|
+    next unless target.name == 'fmt'
+
+    target.build_configurations.each do |config|
+      definitions = config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] || ['$(inherited)']
+      definitions = [definitions] if definitions.is_a?(String)
+      definitions = definitions.dup
+
+      unless definitions.include?('FMT_USE_CONSTEVAL=0')
+        definitions << 'FMT_USE_CONSTEVAL=0'
+      end
+
+      config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] = definitions
+    end
+  end
+end
 `;
 
 function ensurePodfileSetup(src) {
@@ -62,7 +101,8 @@ function ensurePodfileSetup(src) {
 
 function ensurePostInstallAssertion(src) {
   const assertionLine = "    assert_progressive_blur_codegen!(__dir__)\n";
-  if (src.includes(assertionLine)) {
+  const fmtWorkaroundLine = "    apply_fmt_xcode_workaround!(installer)\n";
+  if (src.includes(assertionLine) && src.includes(fmtWorkaroundLine)) {
     return src;
   }
 
@@ -78,10 +118,16 @@ function ensurePostInstallAssertion(src) {
     throw new Error("Unable to find react_native_post_install block to inject progressive blur assertion.");
   }
 
-  return src.replace(
+  let next = src.replace(
     reactNativePostInstallNeedle,
-    `${reactNativePostInstallNeedle}\n${assertionLine}`
+    `${reactNativePostInstallNeedle}\n${fmtWorkaroundLine}${assertionLine}`
   );
+
+  if (!next.includes(fmtWorkaroundLine)) {
+    throw new Error("Unable to inject fmt Xcode workaround into Podfile post_install block.");
+  }
+
+  return next;
 }
 
 module.exports = function withProgressiveBlurNativeBuild(config) {
